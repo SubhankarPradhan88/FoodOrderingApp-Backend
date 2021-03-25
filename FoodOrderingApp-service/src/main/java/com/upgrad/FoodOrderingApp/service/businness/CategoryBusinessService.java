b27719251b6da721515b5220bedf3f84547fcc03
jeffgtxjava <jeffgtxjava@gmail.com>
2021-03-26 03:23:35 +0530

    minor code changes

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryBusinessService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryBusinessService.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -16,22 +18,39 @@ public class CategoryBusinessService {
     @Autowired
     private CategoryDao categoryDao;
 
-
+    @Autowired
+    RestaurantBusinessService restaurantBusinessService;
 
     // A Method which is for  getAllCategories endpoint
     public List<CategoryEntity> getAllCategories(){
-        return  categoryDao.getAllCategories();
+        return categoryDao.getAllCategories().stream()
+                .sorted(Comparator.comparing(CategoryEntity::getCategoryName))
+                .collect(Collectors.toList());
     }
 
     // A Method which takes the categoryUUId as parameter for  getCategoryEntityByUUId endpoint
     public CategoryEntity getCategoryEntityByUuid(final String categoryUUId) throws CategoryNotFoundException {
 
-        // Throw exception if path variable(category_id) is empty
-        if(categoryUUId == null || categoryUUId.isEmpty() || categoryUUId.equalsIgnoreCase("\"\"")){
+        if (categoryUUId.equals("")) {
             throw new CategoryNotFoundException("CNF-001", "Category id field should not be empty");
         }
 
-        return  categoryDao.getCategoryByUUId(categoryUUId);
+        CategoryEntity categoryEntity = categoryDao.getCategoryByUUId(categoryUUId);
+
+        if (categoryEntity == null) {
+            throw new CategoryNotFoundException("CNF-002", "No category by this id");
+        }
+
+        return categoryEntity;
+    }
+
+
+    //List all categories mapped to a restaurant - list by restaurant UUID
+    public List<CategoryEntity> getCategoriesByRestaurant(String restaurantUUID) {
+        RestaurantEntity restaurantEntity = restaurantBusinessService.getRestaurantByUUID(restaurantUUID);
+        return restaurantEntity.getCategories().stream()
+                .sorted(Comparator.comparing(CategoryEntity::getCategoryName))
+                .collect(Collectors.toList());
     }
 
 }

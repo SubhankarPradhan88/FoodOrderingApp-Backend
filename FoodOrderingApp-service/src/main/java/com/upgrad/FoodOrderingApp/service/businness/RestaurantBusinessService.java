633a78e1455c9c26eab8a20a63aa4d78597cda51
jeffgtxjava <jeffgtxjava@gmail.com>
2021-03-25 21:10:09 +0530

    added bussiness logic to validate the restaurant name and retireve it from the DB

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantBusinessService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/RestaurantBusinessService.java
@@ -2,6 +2,7 @@ package com.upgrad.FoodOrderingApp.service.businness;
 
 import com.upgrad.FoodOrderingApp.service.dao.RestaurantDao;
 import com.upgrad.FoodOrderingApp.service.entity.RestaurantEntity;
+import com.upgrad.FoodOrderingApp.service.exception.RestaurantNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,16 @@ public class RestaurantBusinessService {
 
     public List<RestaurantEntity> getAllRestaurants(){
         return this.restaurantDao.getAllRestaurants();
+    }
+
+
+    public List<RestaurantEntity> getRestaurantsByName(String restaurantName) throws RestaurantNotFoundException {
+
+        // Throw exception if path variable(restaurant_name) is empty
+        if(restaurantName == null || restaurantName.isEmpty() || restaurantName.equalsIgnoreCase("\"\"")){
+            throw new RestaurantNotFoundException("RNF-003", "Restaurant name field should not be empty");
+        }
+        return restaurantDao.getRestaurantsByName(restaurantName);
     }
 
 }
